4d1db2e00ba8148bd37c022be7ebf827bfa63af2
Alaa Ayaad <alaaayad1@outlook.com>
2024-03-21 02:52:55 +0200

    firsstt committ

--- src/components/generalcard/GeneralCard.jsx
+++ src/components/generalcard/GeneralCard.jsx
@@ -6,7 +6,7 @@ export default class GeneralCard extends Component {
     return (
       <div className={styles.container}>
         <div className={styles.cont_image}>
-          <img src={require("../../utils/images/image 1.png")} alt=" product" />
+          <img src={image1} alt=" product" />
           <span className={styles.discount}>
             <p>10% Off</p>
           </span>
